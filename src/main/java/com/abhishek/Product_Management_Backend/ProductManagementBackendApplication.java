adbabfbefd0fc6638818ab8368b1865547926193
Abhishek Totad <abhishektotad17@gmail.com>
2023-07-04 22:34:38 +0530

    backend

---
+++ src/main/java/com/abhishek/Product_Management_Backend/ProductManagementBackendApplication.java
@@ -0,0 +1,15 @@
+package com.abhishek.Product_Management_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  ProductManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagementBackendApplication.class, args);
+
+
+	}
+
+}
---
+++ src/main/java/com/abhishek/Product_Management_Backend/service/ProductService.java
@@ -0,0 +1,18 @@
+package com.abhishek.Product_Management_Backend.service;
+
+import com.abhishek.Product_Management_Backend.model.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+    public Product saveProduct(Product product);
+
+    public List<Product> getAllProduct();
+
+    public  Product getProductById(Integer id);
+
+    public String deleteProduct(Integer id);
+
+    public Product editProduct(Product product,Integer id);
+}
